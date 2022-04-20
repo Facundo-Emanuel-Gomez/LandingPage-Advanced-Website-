90d3f9c31bb5da2d1f1c8f11d68e719035d6a18c
FacuGómez32 <facuegomez@hotmail.com>
2022-04-19 21:10:53 -0300

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="react_header section_padding" id="home">
+      <div className="react_header-content">
+        <h1 className="gradient_text">
+          Let's build something amazing with REACT library
+        </h1>
+        <p>React helps you easily create interactive user interfaces. Design simple views for each state in your app, and React will efficiently update and render the right components when data changes.</p>
+        <div className="react_header-content_input">
+          <input type="email" placeholder="Your Email Adress" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="react_header-content_people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className="react_header-image">
+          <img src={ai} alt="ai" />
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
